dfc828111b36069e8be4c94c5f52758909f24743
Matias Turra <matias.turra@gmail.com>
2024-04-20 09:37:39 -0300

    Add post and get sessions routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,24 @@ import { createUserHandler } from "./controller/user.controller";
 import validate from "./middleware/validateResource";
 import validateResource from "./middleware/validateResource";
 import { createUserSchema } from "./schema/user.schema";
+import {
+  createSessionHandler,
+  getUserSessionsHandler,
+} from "./controller/session.controller";
+import { createSessionSchema } from "./schema/session.schema";
+import requireUser from "./middleware/requireUser";
 
 function routes(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
 
   //USER ROUTES
   app.post("/api/users", validateResource(createUserSchema), createUserHandler);
+  app.post(
+    "/api/sessions",
+    validateResource(createSessionSchema),
+    createSessionHandler
+  );
+  app.get("/api/sessions", requireUser, getUserSessionsHandler);
 }
 
 export default routes;
