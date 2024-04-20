026b9c4a1e6907d407bc18cc607e140a6333cab6
Matias Turra <matias.turra@gmail.com>
2024-04-20 09:37:55 -0300

    Add password validation for users

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,3 +1,4 @@
+import { omit } from "lodash";
 import UserModel, { UserInput } from "../models/user.model";
 
 export async function createUser(input: UserInput) {
@@ -6,4 +7,19 @@ export async function createUser(input: UserInput) {
   } catch (error: any) {
     throw new Error(error.message); // It's better to use error.message to get a readable error message
   }
+}
+
+export async function validatePassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const user = await UserModel.findOne({ email });
+  if (!user) return false;
+  const isValid = await user.comparePassword(password);
+
+  if (!isValid) return false;
+  return omit(user.toJSON(), "password");
 }
